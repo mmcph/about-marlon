1cd6a5747a200d3f85f9784c8388d324a7c56353
Marlon McPherson <mmcpherson5@cnm.edu>
2018-04-09 14:10:55 -0600

    restructured files

--- index.php
+++ index.php
@@ -8,6 +8,6 @@
 		<br>
 		<p>I enjoy gaming in my free time. I also enjoy working with my hands and am interested in carpentry and maybe blacksmithing once I'm living in a place where I can bang an anvil all day. I'm working on becoming a web developer, but in an alternate timeline I'm a voice actor - ask me about my accents and impressions. I can be blunt at times, but I always say what I mean, and vice-versa.</p>
 		<p>Here's something I drew.</p>
-		<img src="DSCF2595.JPG">
+		<img src="images/DSCF2595.JPG">
 	</body>
 </html>
