5a1366e4eded4a8400c0810d5ba944a9b11fae49
Marlon McPherson <mmcpherson5@cnm.edu>
2018-04-09 15:10:46 -0600

    added style dir/file, applied to html

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
-		<meta lang="en">
+		<meta charset="UTF-8">
 		<link rel="stylesheet" href="styles/style.css">
+		<link href="https://fonts.googleapis.com/css?family=Contrail+One" rel="stylesheet">
 	</head>
 	<body>
+		<h1>About &nbsp Marlon</h1>
 		<p>Hi, I'm Marlon McPherson. I was born in Santa Fe and grew up in Los Alamos. I graduated from UNM in 2015 with a bachelor's degree in English writing and literature and a minor in art studio.</p>
-		<br>
-		<p>I enjoy gaming in my free time. I also enjoy working with my hands and am interested in carpentry and maybe blacksmithing once I'm living in a place where I can bang an anvil all day. I'm working on becoming a web developer, but in an alternate timeline I'm a voice actor - ask me about my accents and impressions. I can be blunt at times, but I always say what I mean, and vice-versa.</p>
-		<p>Here's something I drew.</p>
+		<p>I enjoy gaming in my free time. I pretend to know how to play guitar (self-taught pleb) and really enjoy metal and rock music. I also enjoy working with my hands and am interested in carpentry (and maybe blacksmithing once I'm living in a place where I can bang an anvil all day). I'm working on becoming a web developer, but in an alternate timeline I'm a voice actor - ask me about my accents and impressions. I love comedy and am of the opinion that "it's either all OK or none of it is OK", so bear that in mind when I'm laughing at my own shitty jokes. I'm an opinionated guy, but I do try to keep it to myself if possible.</p>
+		<p id="ctrtxt">Here's something I drew.</p>
 		<img id="skullpic" src="images/DSCF2595.JPG">
 	</body>
 </html>
