54681f6a981050b0b47bf1042cd1dc54bb442c4a
Marlon McPherson <mmcpherson5@cnm.edu>
2018-04-09 14:08:20 -0600

    Added project files/initial push

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta lang="en">
+	</head>
+	<body>
+		<p>Hi, I'm Marlon McPherson. I was born in Santa Fe and grew up in Los Alamos. I graduated from UNM in 2015 with a bachelor's degree in English writing and literature and a minor in art studio.</p>
+		<br>
+		<p>I enjoy gaming in my free time. I also enjoy working with my hands and am interested in carpentry and maybe blacksmithing once I'm living in a place where I can bang an anvil all day. I'm working on becoming a web developer, but in an alternate timeline I'm a voice actor - ask me about my accents and impressions. I can be blunt at times, but I always say what I mean, and vice-versa.</p>
+		<p>Here's something I drew.</p>
+		<img src="DSCF2595.JPG">
+	</body>
+</html>
